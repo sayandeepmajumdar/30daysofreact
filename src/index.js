1e7f8f4a4e6d7724e62cc61b34f4bf2df5a11a69
Sayandeep Majumdar <sayandeep.majumdar@volitionllp.com>
2021-11-18 08:58:40 +0530

    Day 1 of React App Development

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//stateless functional component
+// always return JSX
+//have to return something & two component won't have same name
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return <h4>Hello World, Welcome to React World</h4>;
+}
+
+// const Greeting = () => {
+//   return React.createElement("h4", {}, "Hello world");
+// };
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
