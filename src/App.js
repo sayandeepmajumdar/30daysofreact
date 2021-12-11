7cc17831053d7b2f109efedac71540dbb2704a27
Sayandeep Majumdar <sayandeep.majumdar@volitionllp.com>
2021-12-11 07:58:57 +0530

    Day 8: Multiple Inputs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import React from "react";
 // import Setup from "./tutorial/2-useEffect/3-useEffect-fetch-data";
 // import Setup from "./tutorial/3-conditional-rendering/2-short-circuit";
 // import Setup from "./tutorial/3-conditional-rendering/3-show-hide";
-import Setup from "./tutorial/4-forms/1-controlled-inputs";
+// import Setup from "./tutorial/4-forms/1-controlled-inputs";
+// import Setup from "./tutorial/4-forms/2-multiple-inputs";
+import Setup from "./tutorial/5-useRef/1-useRef-basics";
 
 function App() {
   return (
